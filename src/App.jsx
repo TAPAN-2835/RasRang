b04f23ad8747f8bfff3dbbcef22180fc5d76d54d
TAPAN PATEL <tapu2835@gmail.com>
2025-07-17 14:51:20 +0530

    commit

--- src/App.jsx
+++ src/App.jsx
@@ -8,27 +8,19 @@ import About from './components/About.jsx'
 import Art from './components/Art.jsx'
 import Menu from './components/Menu.jsx'
 import Contact from './components/Contact.jsx'
-import { useState } from 'react';
 
 gsap.registerPlugin(ScrollTrigger, SplitText);
 
 const App = () => {
-  const [loading, setLoading] = useState(true);
-
   return (
     <main>
-      {loading && <Preloader onFinish={() => setLoading(false)} />}
-      {!loading && (
-        <>
-          <Navbar />
-          <Hero />
-          <Cocktails />
-          <About />
-          <Art />
-          <Menu />
-          <Contact />
-        </>
-      )}
+      <Navbar />
+      <Hero />
+      <Cocktails />
+      <About />
+      <Art />
+      <Menu />
+      <Contact />
     </main>
   )
 }
